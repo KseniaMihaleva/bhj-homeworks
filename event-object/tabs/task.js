d17cfbaac83bb3c0f57adeca5b6b0303abcac176
KseniaMihaleva <ksemi0@yandex.com>
2026-09-23 23:57:53 +0500

    Update task.js

--- event-object/tabs/task.js
+++ event-object/tabs/task.js
@@ -1,1 +1,14 @@
+document.querySelectorAll('.tabs').forEach((tabs) => {
+    const tabItems = tabs.querySelectorAll('.tab');
+    const contentItems = tabs.querySelectorAll('.tab__content');
 
+    tabItems.forEach((tab, index) => {
+        tab.addEventListener('click', () => {
+            tabItems.forEach((t) => t.classList.remove('tab_active'));
+            contentItems.forEach((c) => c.classList.remove('tab__content_active'));
+
+            tab.classList.add('tab_active');
+            contentItems[index].classList.add('tab__content_active');
+        });
+    });
+});
