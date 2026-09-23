d8b6d6420b092d9f81f3fb1c5eb454e1315fca61
KseniaMihaleva <ksemi0@yandex.com>
2026-09-23 21:37:22 +0500

    Create task.js

---
+++ event-object/dropdown/task.js
@@ -0,0 +1,20 @@
+document.querySelectorAll('.dropdown').forEach((dropdown) => {
+    const value = dropdown.querySelector('.dropdown__value');
+    const list = dropdown.querySelector('.dropdown__list');
+    const items = dropdown.querySelectorAll('.dropdown__item');
+
+    value.addEventListener('click', () => {
+        list.classList.toggle('dropdown__list_active');
+    });
+
+    items.forEach((item) => {
+        item.addEventListener('click', (event) => {
+            event.preventDefault();
+
+            const link = item.querySelector('.dropdown__link');
+            value.textContent = link.textContent.trim();
+
+            list.classList.remove('dropdown__list_active');
+        });
+    });
+});
